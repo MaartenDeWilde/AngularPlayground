5952439d4d285ce9d64a82c7755012b05c828d1e
Maarten De Wilde <maarten.dewilde@gmail.com>
2013-10-09 21:29:09 +0200

    Create something that works on the client, still need server

--- AngularPlayground/js/controllers/booksController.js
+++ AngularPlayground/js/controllers/booksController.js
@@ -1,7 +1,6 @@
 'use strict';
-
 define(['app', 'services/booksService'], function (app) {
     return app.controller('BooksController', function($scope, bookService){
-
+        $scope.books = bookService.get();
     });
 });
--- AngularPlayground/js/services/booksService.js
+++ AngularPlayground/js/services/booksService.js
@@ -1,10 +1,10 @@
 'use strict';
 
 define(['app'], function (app) {
-    app.factory('bookService', function(){
+    app.factory('bookService', function($http){
         return {
             get: function(){
-
+                return [ {name:"Awesome book"}, {name:'IT book'}];
             },
             put : function(){
 
