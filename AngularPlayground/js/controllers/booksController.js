467825cadff35a174dd4b7fd2dc4030ad7015fb6
Maarten De Wilde <maarten.dewilde@hotmail.com>
2013-10-13 15:26:41 +0200

    Add filter and query

--- AngularPlayground/js/controllers/booksController.js
+++ AngularPlayground/js/controllers/booksController.js
@@ -3,6 +3,7 @@ define(['app', 'services/booksService'], function (app) {
     return app.controller('BooksController', function($scope, bookService){
         bookService.get(function (books) {
             $scope.books = books;
+            $scope.orderProp = 'name';
         });
     });
 });
